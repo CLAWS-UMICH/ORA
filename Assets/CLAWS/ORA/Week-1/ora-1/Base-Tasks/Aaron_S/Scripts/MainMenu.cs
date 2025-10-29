6196ef1c57eb48bd69bb94f17eaa9d9e727532db
Aaron <sunaaron07@gmail.com>
2025-10-29 13:44:22 -0400

    Finished Week2

--- Assets/CLAWS/ORA/Week-1/ora-1/Base-Tasks/Aaron_S/Scripts/MainMenu.cs
+++ Assets/CLAWS/ORA/Week-1/ora-1/Base-Tasks/Aaron_S/Scripts/MainMenu.cs
@@ -8,20 +8,62 @@ public class MainMenu : MonoBehaviour
 
     [SerializeField] private GameObject screen2;
 
+    [SerializeField] private GameObject vitals;
+
+    bool vitalsState = false;
+    int screenState = 0;
+
     void Start()
     {
-
+        screen1.SetActive(true);
+        screen2.SetActive(false);
+        vitals.SetActive(false);
     }
 
     public void SwitchToScreen1()
     {
         screen1.SetActive(true);
         screen2.SetActive(false);
+        closeVitals();
+        screenState = 0;
     }
 
     public void SwitchToScreen2()
     {
         screen1.SetActive(false);
         screen2.SetActive(true);
+        closeVitals();
+        screenState = 1;
+    }
+
+    public void toggleVitals()
+    {
+        if (vitalsState)
+        {
+            if (screenState == 0)
+            {
+                screen1.SetActive(true);
+                screen2.SetActive(false);
+            }
+            else if (screenState == 1)
+            {
+                screen1.SetActive(false);
+                screen2.SetActive(true);
+            }
+            closeVitals();
+        }
+        else
+        {
+            screen1.SetActive(false);
+            screen2.SetActive(false);
+            vitals.SetActive(true);
+            vitalsState = true;
+        }
+    }
+    
+    public void closeVitals()
+    {
+        vitals.SetActive(false);
+        vitalsState = false;
     }
 }
